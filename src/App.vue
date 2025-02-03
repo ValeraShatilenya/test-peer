4d78ccba8a422a9baa6452cd37d9b846b4733411
Valera Shatilenya <valerashatilenya2@gmail.com>
2025-02-03 10:59:37 +0300

    Add call buttons

--- src/App.vue
+++ src/App.vue
@@ -34,6 +34,19 @@
         @button-click="sendMess"
       />
     </div>
+
+    <BaseButton @click="callToNode">Вызов</BaseButton>
+    <video ref="myVideo" muted="muted" width="400px" height="auto" />
+    <div v-if="peerCall">
+      Входящий звонок
+      <BaseButton @click="callAnswer">Принять</BaseButton>
+      <!-- <button @click="callcancel">Отклонить</button> -->
+    </div>
+    <div v-if="peerCall && remVideo">
+      Звонок начат...
+      <!-- <button @click="callclose">Завершить звонок</button> -->
+    </div>
+    <video ref="remVideo" width="400px" height="auto" />
   </div>
 </template>
 
@@ -48,17 +61,77 @@ const message = ref("");
 const peer = new Peer();
 const otherPeerId = ref("");
 const peerId = ref("");
+const peerCall = shallowRef(null);
 const conn = shallowRef(null); //переменная, хранящая соединение
 
-peer.on("open", function (p) {
-  peerId.value = p;
-});
+const myVideo = ref(null);
+const remVideo = ref(null);
+
+peer.on("open", (p) => (peerId.value = p));
 
 peer.on("connection", function (c) {
   //входящее соединение...
   conn.value = c;
   initConn();
 });
+
+peer.on("call", (call) => (peerCall.value = call));
+
+async function callAnswer() {
+  const mediaStream = await navigator.mediaDevices.getUserMedia({
+    audio: true,
+    // video: true,
+  });
+
+  const video = myVideo.value;
+
+  peerCall.value.answer(mediaStream); // отвечаем на звонок и передаем свой медиапоток собеседнику
+  //peercall.on ('close', onCallClose); //можно обработать закрытие-обрыв звонка
+  video.srcObject = mediaStream; //помещаем собственный медиапоток в объект видео (чтоб видеть себя)
+  video.onloadedmetadata = function (e) {
+    //запускаем воспроизведение, когда объект загружен
+    video.play();
+  };
+  setTimeout(function () {
+    //входящий стрим помещаем в объект видео для отображения
+    remVideo.value.srcObject = peerCall.value.remoteStream;
+    remVideo.value.onloadedmetadata = function (e) {
+      // и запускаем воспроизведение когда объект загружен
+      remVideo.value.play();
+    };
+  }, 1500);
+}
+
+async function callToNode() {
+  try {
+    //вызов
+    const mediaStream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      // video: true,
+    });
+
+    const video = myVideo.value;
+
+    peerCall.value = peer.call(otherPeerId.value, mediaStream);
+
+    peerCall.value.on("stream", function (stream) {
+      //нам ответили, получим стрим
+      setTimeout(function () {
+        remVideo.value.srcObject = peerCall.value.remoteStream;
+        remVideo.value.onloadedmetadata = function (e) {
+          remVideo.value.play();
+        };
+      }, 1500);
+    });
+    //  peercall.on('close', onCallClose);
+    video.srcObject = mediaStream;
+    video.onloadedmetadata = function (e) {
+      video.play();
+    };
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 function addMess(mess) {
   messages.value.push(mess);
